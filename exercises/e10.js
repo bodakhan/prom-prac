e120d6dc3526854d3f73370de7667f34cea25cb3
bodakhan <cjsmith53191@gmail.com>
2024-03-19 22:18:30 -0400

    e10.js successful

--- exercises/e10.js
+++ exercises/e10.js
@@ -1,17 +1,23 @@
 export const getFirstResolvedPromise = (promises) => {
   //*  write code to pass test ⬇ ️
+  return Promise.any(promises);
 };
 
 export const getFirstPromiseOrFail = (promises) => {
-  //*  write code to pass test ⬇ ️
+  //*  write code to pass test ⬇
+  return Promise.race(promises);
 };
 
 export const getQuantityOfRejectedPromises = (promises) => {
-  //*  write code to pass test ⬇ ️
+  //*  write code to pass test ⬇ 
+  return Promise.allSettled(promises)
+  .then(results => results.filter(result => result.status === 'rejected').length);
 };
 
 export const getQuantityOfFulfilledPromises = (promises) => {
   //*  write code to pass test ⬇ ️
+  return Promise.allSettled(promises)
+  .then(results => results.filter(result => result.status === 'fulfilled').length);
 };
 
 //!  ⬇ ⬇ ⬇ ⬇ Don't Edit This Array ⬇ ⬇ ⬇ ⬇
@@ -45,4 +51,18 @@ export const fetchAllCharactersByIds = async (ids) => {
   // To solve this you must fetch all characters passed in the array at the same time
   // use the `fetchCharacterById` function above to make this work
   //*  write code to pass test ⬇ ️
+  const promises = ids.map(id =>
+    fetchCharacterById(id));
+    
+  
+  return Promise.allSettled(promises)
+    .then(results => {
+      const fulfilledResults = results.filter(result => result.status === 'fulfilled');
+      const characters = fulfilledResults.map(result => result.value);
+      return characters.length === ids.length ? characters : [];
+    })
+    .catch(error => {
+      console.error(error);
+      return [];
+    });
 };
