12497f5bf0ef2261035d60a8616a8d327ecc747b
bodakhan <cjsmith53191@gmail.com>
2024-03-19 21:38:58 -0400

    sturggling with e7.js

--- exercises/e7.js
+++ exercises/e7.js
@@ -19,8 +19,16 @@
  *          }
  */
 
-export function parsePromised() {
+export function parsePromised(json) {
   // Your code goes here...
+  return new Promise((resolve, reject) => {
+    try {
+      JSON.parse;
+      resolve(json);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
 /**
@@ -30,8 +38,9 @@ export function parsePromised() {
  * * logs the message property of the error object
  */
 
-export function onReject() {
+export function onReject(error) {
   // Your code goes here...
+  console.log(error.message);
 }
 
 /**
@@ -46,8 +55,11 @@ export function onReject() {
  * Example: export const promiseHandler = () => return <your code>
  */
 
-export const handlePromise = () => {
+export const handlePromise = (promise) => {
   // Your code goes here...
+  return promise
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err, null))
 };
 
 // === TEST YOURSELF ===
